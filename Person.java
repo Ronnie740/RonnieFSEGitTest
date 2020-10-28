43a0a628d624005d9e66b2e07edf47379c827168
Ronnie Ineza <RINEZA200@gcu.ac.uk>
2020-10-28 22:24:53 +0300

    Person class added getters and setters

--- Person.java
+++ Person.java
@@ -13,6 +13,28 @@ public class Person
 		this.first = First;
 		this. surname = surname;
 	}
-
-
+	public String firstName()
+	{
+		return first;
+	}
+	public String otherName()
+	{
+		return other;		
+	}
+	public String surname()
+	{
+		return surname;
+	}
+	pubic void firstName(String firstName)
+	{
+		first=firstName;
+	}
+	pubic void otherName(String otherName)
+	{
+		other=otherName;
+	}
+	pubic void surname(String surname)
+	{
+		surname=surname;
+	}
 }

65a04aaa6144bc5ac926515a172f336ca0b5296d
Ronnie Ineza <RINEZA200@gcu.ac.uk>
2020-10-28 22:11:43 +0300

    Person class first commit

---
+++ Person.java
@@ -0,0 +1,18 @@
+public class Person
+{
+	String first;
+	String other;
+	String surname;
+	
+	public Person()
+	{
+		
+	}
+	public Person(String First, String surname)
+	{
+		this.first = First;
+		this. surname = surname;
+	}
+
+
+}
